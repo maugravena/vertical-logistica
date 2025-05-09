2b4b20aca5aeef0f643f6d79cf88e08b5d60fa1e
Mauricio Gravena <mauricio.gravena@rebase.com.br>
2025-05-09 18:10:58 -0300

    chore: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,33 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+users = 10.times.map do |n|
+  User.create!(
+    user_id: "#{ (n + 1).to_s.rjust(10, '0') }",
+    name: "User #{n + 1}"
+  )
+end
+
+products = 10.times.map do |n|
+  Product.create!(
+    product_id: "#{ (n + 1).to_s.rjust(10, '0') }",
+    product_value: (n + 1) * 10.50
+  )
+end
+
+10.times do |n|
+  user = users[n % users.size]
+  order = Order.create!(
+    user: user,
+    order_id: "#{ (n + 1).to_s.rjust(10, '0') }",
+    purchase_date: Date.today - n.days
+  )
+
+  number_of_items = rand(2..5)
+  number_of_items.times do |i|
+    product = products[(n + i) % products.size]
+    OrderItem.create!(
+      order: order,
+      product: product
+    )
+  end
+end
+
+puts ":: Database Seeded! ::"
